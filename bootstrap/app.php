1124ecd969dc6725dca77f1255211186e7919a7c
americapan <americapan@vip.qq.com>
2026-05-10 19:30:09 +0800

    refactor: 移除未使用的SecurityHeaders中间件

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\SecurityHeaders;
 use App\Http\Middleware\VisitLogMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            SecurityHeaders::class,
             VisitLogMiddleware::class,
         ]);
     })
